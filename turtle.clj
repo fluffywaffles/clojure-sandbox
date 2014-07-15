da7c7b5b687df665e9fc0ed4a2936cf99856ff82
skorlir <skorlir@gmail.com>
2014-07-15 17:52:57 -0500

    generalize kw-getter to just plain getter

--- turtle.clj
+++ turtle.clj
@@ -8,23 +8,31 @@
 
 ;; only use macros when absolutely necessary
 
-(defn keyword->str
+(defn ->str
   "keyword->string, dropping the leading :"
-  [k] (->> k str rest (apply str)))
+  [k]
+  (if (= (type k) clojure.lang.Keyword)
+    (->> k str rest (apply str))
+    (str k)))
+
+(type :a)
+
+(->str \s)
+(->str :j)
 
 (defn dashify [lst]
   "list -> string (with dashes in place of spaces)"
   (->> lst (interleave (repeat \-)) rest (apply str)))
 
 (defn sym-key-prop [k & p]
-  (let [x (map keyword->str (conj p k))]
+  (let [x (map ->str (conj p k))]
     (-> x dashify symbol)))
 
 (sym-key-prop :get :graph :info)
 (sym-key-prop :get prop)
 
 (defmacro getter-example-1 [prop]
-  `(list (symbol (str "get-" (keyword->str prop)))))
+  `(list (symbol (str "get-" (->str prop)))))
 
 (macroexpand (getter-example-1 prop))
 
@@ -36,33 +44,38 @@
 (getter-example-2 prop)
 (getter-example-2 :some-property)
 
-(defmacro kw-getter [prop]
-  `(list 'defn (sym-key-prop :get ~prop) [] ~prop))
+(defmacro fn-er [prop prefix]
+  `(list 'defn (sym-key-prop ~prefix ~prop) [] ~prop))
+
+(defmacro getter [prop]
+  (fn-er prop :get))
 ;; Bah hahahahaha! you can offset the evaluation of the defn, thereby
 ;; ignoring its whole "oh, I need a symbol as my first argument ah-derp"
 ;; situation.
 
 ;; now for a setter...
 
-((eval (kw-getter prop)))
+((eval (getter prop)))
 
-(kw-getter :potato)
-(kw-getter :id)
+(getter :potato)
+(getter 'id)
 
 (get-id)
 
-(macroexpand '(kw-getter :a))
+(macroexpand '(getter :a))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def default-props '(:id :breed :world :color :heading :xcor :ycor :label :size :hidden))
-
-(def sym-props (map #(sym-key-prop :get %) default-props))
+(def default-props '(:id :breed :world :color :heading :xcor :ycor :label :size :hidden?))
 
 (macroexpand '(kw-getter (first default-props)))
-(kw-getter (first default-props))
+(getter (first default-props))
 (get-id)
 
-(map eval (map #(kw-getter %) default-props))
+(map eval (map #(getter %) default-props))
 
 (get-breed)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
