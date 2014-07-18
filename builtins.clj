0d09b07d4a615703f0c41de8c18d6b12274e8317
skorlir <skorlir@gmail.com>
2014-07-18 17:12:39 -0500

    translated builtins.coffee

---
+++ builtins.clj
@@ -0,0 +1,16 @@
+(ns builtins)
+
+(def turtles [:id :color :heading :xcor :ycor :shape :label
+              :label-color :breed :hidden? :size :pen-size
+              :pen-mode])
+
+(def patches [:pxcor :pycor :pcolor :plabel :plabel-color])
+
+(def links [:end1 :end2 :lcolor :llabel :llabelcolor :lhidden
+            :lbreed :thickness :lshape :tiemode])
+
+;; NOTE: this are fns on link; kept here for reference
+;; renderer expects these; might have to add them to an adapter
+;; if we even get to that point (7/18/2014)
+(def links-ex [:color :heading :shape :label :label-color :breed
+               :hidden? :size :midpointx :midpointy])
