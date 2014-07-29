218cf5f74c4d9ad0dfd0a6543efe5578eb30f59a
skorlir <skorlir@gmail.com>
2014-07-29 16:31:50 -0500

    update macros (memoizer)

--- tortoise-cljs/src/tortoise_cljs/util/macros.clj
+++ tortoise-cljs/src/tortoise_cljs/util/macros.clj
@@ -9,10 +9,21 @@
                     (replace {\_ \-})
                     clojure.string/join))
 
-(defmacro memoizer [name-gen fun]
-   `(list 'def
-         (~name-gen ~fun)
-         `(memoize ~~fun)))
+(defmacro memoizer
+  ([name-gen fun nsp]
+     `(def (~name-gen ~fun)
+        `(memoize (symbol (str ~~nsp ~~fun)))))
+  ([fun nsp]
+    (let [ nfn #(symbol (_to- %))]
+      `(def ~(nfn fun) (memoize ~(symbol (str nsp "/" fun))))))
+  ([fun]
+    (let [ nfn #(symbol (_to- %)) ]
+      `(def ~(nfn fun) (memoize ~fun)))))
 
-(defmacro add-patch-math-fn [f]
-  `(def ~f ~(symbol (str "topology.patch-math/" f))))
+(macroexpand '(memoizer _get_patch_north topology.patch-math))
+
+(defmacro add-patch-math-fn
+  ([f]
+   `(def ~f ~(symbol (str "topology.patch-math/" f))))
+  ([f nm]
+    `(def ~nm ~(symbol (str "topology.patch-math/" f)))))
