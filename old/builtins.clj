8202680bdb5c494655931f297508685833e2b7c9
skorlir <skorlir@gmail.com>
2014-08-05 12:32:28 -0500

    retroactive grammar correction

--- old/builtins.clj
+++ old/builtins.clj
@@ -9,7 +9,7 @@
 (def links [:end1 :end2 :lcolor :llabel :llabelcolor :lhidden
             :lbreed :thickness :lshape :tiemode])
 
-;; NOTE: this are fns on link; kept here for reference
+;; NOTE: these are fns on link; kept here for reference
 ;; renderer expects these; might have to add them to an adapter
 ;; if we even get to that point (7/18/2014)
 (def links-extra [:color :heading :shape :label :label-color :breed
